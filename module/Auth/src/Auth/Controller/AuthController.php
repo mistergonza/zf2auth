d95866feb3a8dce5e138fe7fe891952b5475f1e7
GonZa <GonZa@GonZa-PC>
2014-05-14 22:13:21 +0400

    Add login form

--- module/Auth/src/Auth/Controller/AuthController.php
+++ module/Auth/src/Auth/Controller/AuthController.php
@@ -6,11 +6,12 @@ namespace Auth\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
+use Zend\Authentication\Adapter\DbTable as AuthAdapter;
 
 class AuthController extends AbstractActionController
 {
     public function loginAction()
     {
-        
+        return new ViewModel();
     }
 }
---
+++ module/Auth/src/Auth/Form/Login.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Login Form
+ * 
+ * @author a.ludvikov <gonza.work@gmail.com>
+ */
+namespace Auth\Form;
+
+use Zend\Form\Form;
+
+class Login extends Form
+{
+   public function __construct($name = null)
+    {
+        parent::__construct('login');
+        $this->setAttribute('method', 'post');
+        $this->add(array(
+            'name' => 'login',
+            'attributes' => array(
+                'type'  => 'text',
+            ),
+            'options' => array(
+                'label' => 'Login',
+            ),
+        ));
+        $this->add(array(
+            'name' => 'password',
+            'attributes' => array(
+                'type'  => 'password',
+            ),
+            'options' => array(
+                'label' => 'Password',
+            ),
+        ));
+        $this->add(array(
+            'name' => 'submit',
+            'attributes' => array(
+                'type'  => 'submit',
+                'value' => 'OK',
+                'id' => 'submitbutton',
+            ),
+        ));
+    }
+
+}
